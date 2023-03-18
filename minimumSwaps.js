cba0b3c304e169d1ff93cb418cff21c85effeef1
ChristineGoGo <brandywang22@gmail.com>
2023-03-18 10:21:56 -0700

    minimumSwaps.js

--- minimumSwaps.js
+++ minimumSwaps.js
@@ -3,12 +3,17 @@ function minimumSwaps(arr) {
     let l = arr.length - 1;
     let a = [];
     
-    while (l > 0 ) { 
+    while (l > 0 ) {
         let v = Math.min(...arr);
         a.push(v);
-        arr.remove(v);
+        arr.splice(arr[l], 1);
+        console.log(arr);
         m++;
         l--;
     }
+    console.log(a);
     return m;
 }
+let arr = [7, 1, 3, 2, 4, 5, 6];
+console.log(minimumSwaps(arr));
+
